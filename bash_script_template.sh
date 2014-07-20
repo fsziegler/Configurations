f5bac691c62473352b992ef93e9a953c09a0851a
fsziegler <fsziegler@gmail.com>
2014-07-20 12:16:18 -0700

    Update bash_script_template.sh
    Update, tested

--- bash_script_template.sh
+++ bash_script_template.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # Adapted from http://tldp.org/HOWTO/Bash-Prog-Intro-HOWTO.html
-# 1. Always startBash shell scripts  with "#!/bin/bash" on the first line
+# 1. Always start Bash shell scripts  with "#!/bin/bash" on the first line
 # 2. Comments start with "#"
-# 3. run `hod +x bash_script_template.sh` to k thi rit
+# 3. Run `chmod +x bash_script_template.sh` to make this script runnable
+# 4. Run ./bash_script_template.sh to execute this script
 
-# Variables
+echo Variables
 STR="Hello World!"
 echo $STR
 
-# Local variables
+echo 
+echo Local variables
 HELLO=Hello 
 function hello {
     local HELLO=World
@@ -18,7 +20,8 @@ echo $HELLO
 hello
 echo $HELLO
 
-# Basic if .. then .. else
+echo 
+echo Basic if .. then .. else
 T1="foo"
 T2="bar"
 if [ "$T1" = "$T2" ]; then
@@ -27,35 +30,41 @@ else
     echo expression evaluated as false
 fi
 
-# For loop #1, bash-style
+echo 
+echo For loop #1, bash-style
 # Iterates over a series of "words" in a string
 for i in $( ls ); do
   echo item: $i
 done
 
-# For loop #2, C-style
+echo 
+echo For loop #2, C-style
 for i in `seq 1 10`;
 do
   echo $i
 done  
 
-# While loop
+echo 
+echo While loop
 COUNTER=0
 while [  $COUNTER -lt 10 ]; do
    echo The counter is $COUNTER
    let COUNTER=COUNTER+1 
 done
-         
-# Until loop     
+      
+echo          
+echo Until loop     
 COUNTER=20
 until [  $COUNTER -lt 10 ]; do
    echo COUNTER $COUNTER
    let COUNTER-=1
 done
 
-# Functions
+echo 
+echo Functions
 function quit {
-  exit
+  echo "(exit)"
+#  exit
 }
 function hello {
   echo Hello!
@@ -65,8 +74,9 @@ quit
 echo foo 
 
 function quit {
-   exit
-}  
+  echo "(exit)"
+#  exit
+}
 function e {
     echo $1 
 }  
@@ -75,3 +85,49 @@ e World
 quit
 echo foo
 
+echo 
+echo Simple menus
+OPTIONS="Hello Quit"
+select opt in $OPTIONS; do
+#read -p "Enter menu command:"
+if [ "$opt" = "Quit" ]; then
+  echo "(exit)"
+#  exit
+   break
+elif [ "$opt" = "Hello" ]; then
+   echo Hello World
+else
+   clear
+   echo bad option
+fi
+done
+
+echo 
+echo Using the command line
+if [ -z "$1" ]; then 
+  echo usage: $0 directory
+#  exit
+fi
+SRCD=$1
+TGTD="/var/backups/"
+OF=home-$(date +%Y%m%d).tgz
+echo Result: "tar -cZf $TGTD$OF $SRCD"
+
+echo 
+echo Reading user input with read
+echo Please, enter your name
+read NAME
+echo "Hi $NAME!"
+        
+echo Please, enter your firstname and lastname
+read FN LN 
+echo "Hi! $LN, $FN !"
+        
+echo 
+echo Arithmetic evaluation
+echo "echo 1 + 1:     " 1 + 1
+echo "echo \$((1+1)):  " $((1+1))
+echo "echo \$[1+1]:    " $[1+1]
+echo "echo \$[3/4]:    " $[3/4] 
+echo "echo 3/4|bc -l: [next line]" 
+echo 3/4 | bc -l
