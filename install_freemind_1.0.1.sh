8f5ec2e8c4b244c39bcf727530bb262eaf325337
fsziegler <fsziegler@gmail.com>
2015-09-26 13:14:27 -0700

    Corrected Freeminf image manipulation

--- install_freemind_1.0.1.sh
+++ install_freemind_1.0.1.sh
@@ -24,8 +24,8 @@ pushd .
 cd /usr/bin && sudo ln -s /usr/share/freemind/freemind.sh /usr/bin/freemind
 sudo apt-get install -y desktop-file-utils
 cd /usr/share/applications && sudo mv freemind.desktop.bak freemind.desktop && sudo desktop-file-install freemind.desktop
-popd
-sudo jar xf ./freemind/lib/freemind.jar images/FreeMindWindowIcon.png
+sudo jar xf /usr/share/freemind/lib/freemind.jar images/FreeMindWindowIcon.png
 sudo mv ./images/FreeMindWindowIcon.png /usr/share/pixmaps
 sudo rm -rf ./images
+popd
 echo "=== Please note that it often takes a few seconds before FreeMind shows up under Applications->Office"
