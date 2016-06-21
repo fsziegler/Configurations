3b183468b102fc835eae555220372906ab64318d
fsziegler <fsziegler@gmail.com>
2016-06-20 23:29:51 -0700

    Create setup_postfix.sh
    Added script to install, set up, and test postfix email

---
+++ setup_postfix.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo See "https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-postfix-as-a-send-only-smtp-server-on-ubuntu-14-04" \
+for instructions on configuring a mail server
+read -n 1 -s
+sudo apt-get install mailutils
+sudo nano /etc/postfix/main.cf
+sudo service postfix restart
+echo "This is the body of the email" | mail -s "This is the subject line" [your email]@gmail.com
