d78f0f94f44074450e45a9846ed117d2525c798c
fred <fsziegler@gmail.com>
2016-06-22 11:01:26 -0700

    More fixes for install_qt5_7.sh

--- install_qt5_7.sh
+++ install_qt5_7.sh
@@ -5,8 +5,7 @@ cd ~/dev/qt5_7;
 #wget http://download.qt.io/official_releases/qt/5.4/5.4.2/single/qt-everywhere-opensource-src-5.4.2.tar.gz
 wget http://download.qt.io/official_releases/qt/5.7/5.7.0/single/qt-everywhere-opensource-src-5.7.0.tar.gz
 sudo apt-get install make
+sudo apt-get -y install g++
 tar xvf qt-everywhere-opensource-src-5.7.0.tar.gz
 cd qt-everywhere-opensource-src-5.7.0/
-./configure -qt-xcb
-make
-sudo make install
+echo "Run ./configure -qt-xcb, then make, then sudo make install"
