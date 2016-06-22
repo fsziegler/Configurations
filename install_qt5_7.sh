17acca33fc39ac1db35c5055a687244ddc461c2b
fred <fsziegler@gmail.com>
2016-06-22 10:58:27 -0700

    Added install make

--- install_qt5_7.sh
+++ install_qt5_7.sh
@@ -4,6 +4,7 @@ mkdir ~/dev/qt5_7;
 cd ~/dev/qt5_7;
 #wget http://download.qt.io/official_releases/qt/5.4/5.4.2/single/qt-everywhere-opensource-src-5.4.2.tar.gz
 wget http://download.qt.io/official_releases/qt/5.7/5.7.0/single/qt-everywhere-opensource-src-5.7.0.tar.gz
+sudo apt-get install make
 tar xvf qt-everywhere-opensource-src-5.7.0.tar.gz
 cd qt-everywhere-opensource-src-5.7.0/
 ./configure -qt-xcb
