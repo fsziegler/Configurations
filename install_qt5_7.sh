4dee263a1aa499e0744239369ae1139ed6c0bab2
fsziegler <fsziegler@gmail.com>
2016-06-22 10:11:18 -0700

    Create install_qt5_7.sh
    Bash script to download and build Qt 5.7

---
+++ install_qt5_7.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir ~/dev;
+mkdir ~/dev/qt5_7;
+cd ~/dev/qt5_7;
+#wget http://download.qt.io/official_releases/qt/5.4/5.4.2/single/qt-everywhere-opensource-src-5.4.2.tar.gz
+wget http://download.qt.io/official_releases/qt/5.7/5.7.0/single/qt-everywhere-opensource-src-5.7.0.tar.gz
+cd qt-everywhere-opensource-src-5.7.0/
+./configure -qt-xcb
+make
+sudo make install
