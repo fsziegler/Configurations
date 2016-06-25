4d8465cf8aa4ee348fa1eb519afcf9b2e3e0e2d9
fsziegler <fsziegler@gmail.com>
2016-06-25 11:45:17 -0700

    Update setup_postfix.sh
    small improvement

--- setup_postfix.sh
+++ setup_postfix.sh
@@ -2,7 +2,7 @@
 echo See "https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-postfix-as-a-send-only-smtp-server-on-ubuntu-14-04" \
 for instructions on configuring a mail server
 read -n 1 -s
-sudo apt-get install mailutils
+sudo apt-get -y install mailutils
 sudo nano /etc/postfix/main.cf
 sudo service postfix restart
 echo "This is the body of the email" | mail -s "This is the subject line" [your email]@gmail.com
